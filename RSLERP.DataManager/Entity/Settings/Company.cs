14aad0a2cff2a37034f2788f97e6146bd9dafdc5
sadiredwan <sadi.redwan@gmail.com>
2019-06-13 12:34:08 +0600

    s_Company, s_Departments name validation.

--- RSLERP.DataManager/Entity/Settings/Company.cs
+++ RSLERP.DataManager/Entity/Settings/Company.cs
@@ -18,6 +18,7 @@ namespace RSLERP.DataManager.Entity
         public int c_g_ID { get; set; }
 
         [Required]
+        [CompanyNameValidation(ErrorMessage = "Company already exists")]
         public String c_Name { get; set; }
         public String c_Address { get; set; }
         public String c_FactoryAddress { get; set; }
@@ -31,7 +32,41 @@ namespace RSLERP.DataManager.Entity
         public int? CompanyId { get; set; }
         public DateTime? created_at { get; set; }
         public int? modified_by { get; set; }
-        public DateTime? modified_at { get; set; }        public int? app_id { get; set; }
+        public DateTime? modified_at { get; set; }        public int? app_id { get; set; }
+
+
+        [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+        public class CompanyNameValidation : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                if (!(value is string)) return new ValidationResult("DataType Error");
+                var val = (string)value;
+                try
+                {
+                    var idProperty = validationContext.ObjectInstance.GetType().GetProperty("c_ID");
+                    var idPropertyValue = idProperty.GetValue(validationContext.ObjectInstance, null);
+                    if (idPropertyValue.ToString() != "0")
+                    {
+                        return ValidationResult.Success;
+                    }
+                    var findCompanyName = new DBContext().Companies.ToList().FindAll(x => x.c_Name == val);
+                    if (findCompanyName.Count > 0)
+                    {
+                        return new ValidationResult(FormatErrorMessage(validationContext.MemberName));
+                    }
+                }
+                catch (Exception e)
+                {
+                    return ValidationResult.Success;
+                }
+                return ValidationResult.Success;
+            }
+            public override string FormatErrorMessage(string name)
+            {
+                return base.FormatErrorMessage(name);
+            }
+        }
     }
 
 
--- RSLERP.DataManager/Entity/Settings/Department.cs
+++ RSLERP.DataManager/Entity/Settings/Department.cs
@@ -16,11 +16,46 @@ namespace RSLERP.DataManager.Entity
         [Key]
         [Required]
         public int id { get; set; }
+        [Required]
+        [DepartmentNameValidation(ErrorMessage = "Department already exists")]
         public string name { get; set; }
         public string description { get; set; }        public int? created_by { get; set; }
         public int? CompanyId { get; set; }
         public DateTime? created_at { get; set; }
         public int? modified_by { get; set; }
-        public DateTime? modified_at { get; set; }        public int? app_id { get; set; }
+        public DateTime? modified_at { get; set; }        public int? app_id { get; set; }
+        
+        [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+        public class DepartmentNameValidation : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                if (!(value is string)) return new ValidationResult("DataType Error");
+                var val = (string)value;
+                try
+                {
+                    var idProperty = validationContext.ObjectInstance.GetType().GetProperty("id");
+                    var idPropertyValue = idProperty.GetValue(validationContext.ObjectInstance, null);
+                    if (idPropertyValue.ToString() != "0")
+                    {
+                        return ValidationResult.Success;
+                    }
+                    var findDepartmentName = new DBContext().Departments.ToList().FindAll(x => x.name == val);
+                    if (findDepartmentName.Count > 0)
+                    {
+                        return new ValidationResult(FormatErrorMessage(validationContext.MemberName));
+                    }
+                }
+                catch (Exception e)
+                {
+                    return ValidationResult.Success;
+                }
+                return ValidationResult.Success;
+            }
+            public override string FormatErrorMessage(string name)
+            {
+                return base.FormatErrorMessage(name);
+            }
+        }
     }
 }
