adcd91cd6eec12e65ab3fe1452aa97dc1e7e543b
sadiredwan <sadi.redwan@gmail.com>
2019-06-17 10:23:07 +0600

    BankInfo Init.

---
+++ RSLERP.DataManager/Entity/Settings/BankInfo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RSLERP.DataManager.Entity
+{
+    [Table("s_BankInfos")]
+    public class BankInfo : IBaseModel
+    {
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int id { get; set; }
+        [Required]
+        [BankNameValidation(ErrorMessage = "Bank already exists.")]
+        public String name { get; set; }
+        [Required]
+        public String short_name { get; set; }
+        public String address { get; set; }
+        [Required]
+        public int type { get; set; }
+        [Required]
+        public int country_id { get; set; }
+        [Required]
+        public bool status { get; set; }
+        public int? created_by { get; set; }
+        public DateTime? created_at { get; set; }
+        public int? modified_by { get; set; }
+        public DateTime? modified_at { get; set; }
+        public int? CompanyId { get; set; }
+        public int? app_id { get; set; }
+
+
+        [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+        public class BankNameValidation : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                if (!(value is string)) return new ValidationResult("DataType Error");
+                var val = (string)value;
+                try
+                {
+                    var idProperty = validationContext.ObjectInstance.GetType().GetProperty("id");
+                    var idPropertyValue = idProperty.GetValue(validationContext.ObjectInstance, null);
+                    if (idPropertyValue.ToString() != "0")
+                    {
+                        return ValidationResult.Success;
+                    }
+                    var findBankName = new DBContext().BankInfos.ToList().FindAll(x => x.name == val);
+                    if (findBankName.Count > 0)
+                    {
+                        return new ValidationResult(FormatErrorMessage(validationContext.MemberName));
+                    }
+                }
+                catch (Exception e)
+                {
+                    return ValidationResult.Success;
+                }
+                return ValidationResult.Success;
+            }
+            public override string FormatErrorMessage(string name)
+            {
+                return base.FormatErrorMessage(name);
+            }
+        }
+    }
+        
+}
