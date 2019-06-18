6a5852e84a8c423fcba5af702d9ab1b6ab7f968a
rahuldas <das063020@gmail.com>
2019-06-18 10:43:19 +0600

    dfdfdf

---
+++ RSLERP.DataManager/Entity/AccountFinance/AnFCOA.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSLERP.DataManager.Entity
+{
+    [Table("AnFCOAs")]
+    public class AnFCOA : IBaseModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public long Id { get; set; }
+        public long? AnFCOAId { get; set; }
+        public int? CmnCompanyId { get; set; }
+        public String Code { get; set; }
+        public String UnsedCode { get; set; }
+        public int? DepthLevel { get; set; }
+        public int? NotesNo { get; set; }
+        public int? BLorPL { get; set; }
+        public String Particular { get; set; }
+        public String SubTitles { get; set; }
+        public String Address { get; set; }
+        public String AccNo { get; set; }
+        public String ContactNo { get; set; }
+        public bool? Status { get; set; }
+        public bool? IsTransactionalHead { get; set; }
+        public bool? IsFactoryAccess { get; set; }
+        public int? CashOrBank { get; set; }
+        public int? created_by { get; set; }
+        public DateTime? created_at { get; set; }
+        public int? modified_by { get; set; }
+        public DateTime? modified_at { get; set; }        public int? CompanyId { get; set; }
+        public int? app_id { get; set; }
+    }
+}
---
+++ RSLERP/Controllers/AccountFinance/Settings/ChartAccountController.cs
@@ -0,0 +1,51 @@
+﻿using RSLERP.DataManager;
+using RSLERP.DataManager.Entity;
+using RSLERP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RSLERP.Controllers.AccountFinance.Settings
+{
+    public class ChartAccountController : Controller
+    {
+        ViewModel vmdl = new ViewModel();
+        // GET: ChartAccount
+        public ActionResult Index()
+        {
+            vmdl.VM_ROLEPERMISSIONS = new List<RolePermission>();
+            List<AnFCOA> sscolParent = new DBContext().AnFCOAs.ToList();
+            List<TreeMenu> lstTreeMenu = makeTreeMenuCOA(0, sscolParent);
+            vmdl.VM_TREE_MENUS = lstTreeMenu;
+            
+            return View(vmdl);
+        }
+
+        public List<TreeMenu> makeTreeMenuCOA(long parentid, List<AnFCOA> allmenuLst)
+        {
+            List<TreeMenu> menuLst = new List<Models.TreeMenu>();
+            List<AnFCOA> srchLstbyParentID = allmenuLst.FindAll(x => (x.AnFCOAId == null ? 0 : x.AnFCOAId) == parentid);
+            foreach (AnFCOA moduleMdl in srchLstbyParentID)
+            {
+                TreeMenu treemenuMdl = new TreeMenu();
+                treemenuMdl.Link_Name = moduleMdl.Code + " - " + moduleMdl.Particular;
+                treemenuMdl.Link_Url = "" + moduleMdl.Id;
+
+                treemenuMdl.childTreeLst = makeTreeMenuCOA((long)moduleMdl.Id, allmenuLst);
+                menuLst.Add(treemenuMdl);
+            }
+
+            return menuLst;
+        }
+
+
+        public PartialViewResult PartialLoadCOA(String id)
+        {
+           
+
+            return PartialView(vmdl);
+        }
+    }
+}
